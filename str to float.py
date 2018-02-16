434ab027dce365f1acd42a4407f0b95305e705f2
Raj <rajmtech17@gmail.com>
2018-02-16 22:45:45 +0530

    i know str to float...

---
+++ str to float.py
@@ -0,0 +1,14 @@
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 2
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float=float(str(pi_string))
+print(pi_float)
+print(type(pi_float))
