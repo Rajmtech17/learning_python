3c168c2d4a2763ab8734633a14f6551666240395
Raj <rajmtech17@gmail.com>
2018-02-22 15:46:12 +0530

    i know tuble_data_structures

---
+++ code_sheet/tuple_data_structures.py
@@ -0,0 +1,12 @@
+#create tuple variaples
+tup1 = ('physics', 'chemistry', 1997, 2000)
+tup2 = (1, 2, 3, 4, 5 )
+tup3 = "a", "b", "c", "d"
+#print out tuple values
+print ("tup1[0]: ", tup1[0])
+print ("tup2[1:5]: ", tup2[1:5])
+#Concatenate Tuples
+tup4 = tup1 + tup2
+print (tup4)
+
+
